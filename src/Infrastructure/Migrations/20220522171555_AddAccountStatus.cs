d15b9ec3372f8a687e7f5ee55606ac4eb286693b
Kevin Winkel <accounts@kwinkel.one>
2022-05-23 20:57:07 +0200

    commit untracked files

---
+++ src/Infrastructure/Migrations/20220522171555_AddAccountStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELifeRPG.Infrastructure.Migrations
+{
+    public partial class AddAccountStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Account",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Account");
+        }
+    }
+}
